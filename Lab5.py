ad6309c51440e77b18ca57c599829b45f85fceaf
Sana Baloch <denoallah66@gmail.com>
2024-11-05 12:09:34 +0500

    Add files via upload

---
+++ Lab5.py
@@ -0,0 +1,29 @@
+                         # Task : 1
+
+for i in range (1 , 11):
+      print(i)  
+
+                          # Task : 2   
+
+sum = 0
+for i in range(1 , 15):
+    sum += i
+print("The Sum is :" , sum)
+
+                        # Task : 3
+                         
+n = 5
+for i in range(1, n + 1):
+    for j in range(i):
+        print("*", end=" ")
+    print()
+
+                # Task : 4
+
+
+print("Even numbers are:")
+for i in range(1, 21):
+    if i % 2 == 0:
+        print(i)
+
+                                                  
